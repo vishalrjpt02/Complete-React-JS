58c84c3ce746adfa7c5573a08710ba16a4b049a4
Vishal Rajput <vishalrjpt02@outlook.in>
2024-03-05 01:02:13 +0530

    react with bable overview

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,9 +3,41 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 
-ReactDOM.createRoot(document.getElementById('root'))
-.render(
-  
-    <App />
-  ,
+function Myapp(){
+  return (
+    <div>
+      <h1>custome app !</h1>
+      </div>
+  )
+}
+
+
+// Fails coz Bables doesn't identifies the sintex
+
+// const reactElement = {
+//   type : 'a',
+//   props : {
+//       href : 'https://google.com',
+//       target : '_blank'
+//   },
+//   children : 'Click me to visit google'
+// }
+
+
+const anathorElement = (
+  <a href='https//google.com' target='_blank'>Visit Google</a>
+)
+
+// let's dot it with right sintex 
+// as Babale says (https://github.com/facebook/react/blob/main/packages/react/src/jsx/ReactJSXElement.js)
+const reactElement =React.createElement(
+  'a',
+  {href : 'https://google.com', target: '_blank'},
+  'click me to visit google'
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+  reactElement
 );
--- Custom React/customReact.js
+++ Custom React/customReact.js
@@ -1,12 +1,21 @@
 function customerRander(reactElement, container){
-const domElement = document.createElement
-(reactElement.type)
-domElement.innerHTML = reactElement.children
-domElement.setAttribute('href', reactElement.props.href )
-domElement.setAttribute('target' , reactElement.props.target)
+    /*const domElement = document.createElement
+        (reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href )
+    domElement.setAttribute('target' , reactElement.props.target)
 
-container.appendChild(domElement)
+    container.appendChild(domElement)
+    */
 
+    const domElement =document.createElement
+        (reactElement.type)
+    domElement.innerHTML =reactElement.children
+    for (const prop in reactElement.props) {
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
 }
 
 const reactElement = {
