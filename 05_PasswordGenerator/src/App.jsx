98ecca2537136ac6411267dbe355aa9c69b1b5f6
Vishal Rajput <vishalrjpt02@outlook.in>
2024-04-16 19:39:18 +0530

    finally completed this project

--- 05_PasswordGenerator/src/App.jsx
+++ 05_PasswordGenerator/src/App.jsx
@@ -2,25 +2,40 @@ import { useState , useCallback, useEffect, useRef } from 'react'
 
 
 function App() {
-  const [lenght, setLenght] = useState(8);
+  const [length, setlength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setpassword] = useState("");
 
+  //use Ref
+  const passwordRef = useRef(null);
+   
   const passwordGenrator = useCallback( () => {
     let pass ="";
     let str ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
     if(numberAllowed) str += '0123456789';
-    if(charAllowed) str+= "!@#$%^&*()~_+{}[]~'";
+    if(charAllowed) str += "!@#$%^&*()~_+{}[]~'";
 
     for (let i = 0; i <= length; i++) {
       let char =Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
     setpassword(pass)
   } 
-  , [lenght,numberAllowed,charAllowed,password, setpassword])
+  , [length,numberAllowed,charAllowed, setpassword])
+
+  const copyPasswordClipBoard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,20);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+
+  useEffect(() => {
+     passwordGenrator()}, 
+  [length,numberAllowed,charAllowed,passwordGenrator] )
+  
   return (
 
     <div>
@@ -32,18 +47,21 @@ function App() {
                 className="outline-none w-full py-1 px-3" 
                 placeholder= "password" 
                 readonly 
+                ref={passwordRef}
         /> 
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+          <button 
+          onClick={copyPasswordClipBoard}
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
         </div>
       <div className='flex text-sm gap-x-2'>
         <div className='flex items-centre gap-x-1'>
           <input type="range"
           min={6}
           max={100}
-          value={lenght}
+          value={length}
           className='cursor-pointer' 
-          onChange={(e) => {setLenght(e.target.value)}} />
-          <label>Lenght : {lenght}</label>
+          onChange={(e) => {setlength(e.target.value)}} />
+          <label>length : {length}</label>
         </div>
           <div className='flex items-centre gap-x-1'>
             <input 
